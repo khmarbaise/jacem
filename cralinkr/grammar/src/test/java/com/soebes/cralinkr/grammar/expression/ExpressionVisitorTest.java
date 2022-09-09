d2d79ca123aad44e00b0936785ad57b681fa5b1e
Karl Heinz Marbaise <kama@soebes.de>
2022-09-09 23:09:40 +0200

    Improved.

--- cralinkr/grammar/src/test/java/com/soebes/cralinkr/grammar/expression/ExpressionVisitorTest.java
+++ cralinkr/grammar/src/test/java/com/soebes/cralinkr/grammar/expression/ExpressionVisitorTest.java
@@ -45,6 +45,7 @@ class ExpressionVisitorTest {
         arguments("Division", "1024/256", 4L),
         arguments("Check for precedence", "2*(3+4)", 14L),
         arguments("Check for precedence", "2+3*2", 8L),
+        arguments("Check for precedence no change", "2+(3*2)", 8L),
         arguments("Unary simple", "-1024+3072", 2048L),
         arguments("Unary complex", "-1024-(-1024)", 0L),
         arguments("Hex value", "$1000", 4096L),
